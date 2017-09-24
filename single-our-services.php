27715994a258880e94e060ad3d9598ad981f8921
madzathemes <madza@madzathemes.com>
2017-09-25 00:34:42 +0300

    fix

--- single-our-services.php
+++ single-our-services.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author madars.bitenieks
- * @copyright 2013
+ * @copyright 2017
  */
 
 get_header();
--- single-our-staff.php
+++ single-our-staff.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author madars.bitenieks
- * @copyright 2013
+ * @copyright 2017
  */
 
 get_header();
@@ -18,72 +18,74 @@ $src = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), array( 9
 			if(!empty($layouts)){
 				$layout= $layouts;
 			} else {
-				$layout = "full";
+				$layout = "3";
 			}
 			$float_layout = "pull-left";
 			$float_sidebar = "pull-right";
 
-			if ($layout == "left") {
+			if ($layout == "2") {
 				$float_layout = "pull-right";
 				$float_sidebar = "pull-left";
 			}
 
 			$more = 0;
 
-			if ($layout == "full") {
+			if ($layout == "3") {
 			?>
 			<div class="row">
 					<div class="col-md-12">
 
-		<header>
-			<div class="entry-page-image">
+
+			<div class="entry-page-image mt-radius">
 
 					<?php
 
 					if(has_post_thumbnail()) {
 
-						echo get_the_post_thumbnail( get_the_ID(), array('750', '', ''), $src[0] );
+						echo get_the_post_thumbnail( get_the_ID(), array('1300', '', ''), $src[0] );
 
 					}
 
 					?>
 
 			</div>
-		</header>
-		<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+		<div class="mt-content-text">	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 			<?php  the_content(); ?>
 		<?php endwhile; ?>
+	</div>
 </div>
 	</div>
 <?php } else { ?>
 
 <div class="row">
-	<div class="col-md-<?php if ($layout == "full" or $layout == "") { echo "12 ";  } else {  echo "8 ";  } echo sanitize_html_class($float_layout); ?> ">
-		<div class="entry-page-image">
+	<div class="col-md-<?php if ($layout == "3" or $layout == "") { echo "12 ";  } else {  echo "8 ";  } echo sanitize_html_class($float_layout); ?> ">
+		<div class="entry-page-image mt-radius">
 
 				<?php
 
 				if(has_post_thumbnail()) {
 
-					echo get_the_post_thumbnail( get_the_ID(), array('750', '', ''), $src[0] );
+					echo get_the_post_thumbnail( get_the_ID(), array('854', '', ''), $src[0] );
 
 				}
 
 				?>
 
 		</div>
-				<?php if ( have_posts() ) while ( have_posts() ) : the_post();
+		<div class="mt-content-text">		<?php if ( have_posts() ) while ( have_posts() ) : the_post();
 
 					the_content();
 					comments_template( '', true );
 
 				endwhile;  ?>
+			</div>
 
 	</div>
 
-	<?php if ($layout == "full" or $layout == "") {} else { ?>
+	<?php if ($layout == "3" or $layout == "") {} else { ?>
 
-		<div class="col-md-4 sidebar <?php echo sanitize_html_class($mt_float_sidebar); ?> " temscope="itemscope" itemtype="http://schema.org/WPSideBar"><?php get_sidebar(); ?></div>
+		<div class="col-md-4 sidebar <?php echo sanitize_html_class($float_sidebar); ?> " temscope="itemscope" itemtype="http://schema.org/WPSideBar"><?php get_sidebar(); ?></div>
 
 	<?php } ?>
 
