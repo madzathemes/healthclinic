78d3caa9179b83b7cf848022c90b24b71d508a58
madzathemes <madza@madzathemes.com>
2017-10-02 23:49:12 +0300

    fix

--- inc/functions/functions-title.php
+++ inc/functions/functions-title.php
@@ -8,21 +8,21 @@ $subtitle = get_post_meta(get_the_ID(), "madzaplug_sub_title", true);
 					<h1><?php printf( esc_html__( 'Search Results for: %s', "healthclinic"  ), '' . get_search_query() . '' ); ?></h1>
         <?php } else if(is_page()) { ?>
           <h1><?php the_title(); ?></h1>
-          <?php  if(!empty($subtitle)){ ?><h4 class="mt-page-subtitle" itemprop="description"><?php echo $subtitle; ?></h2><?php } ?>
+          <?php  if(!empty($subtitle)){ ?><h4 class="mt-page-subtitle" itemprop="description"><?php echo esc_html($subtitle); ?></h2><?php } ?>
 
         <?php } else if(is_home()) { ?>
           <h1><?php echo esc_html__( 'News', "healthclinic"  ); ?></h1>
-          <?php  if(!empty($subtitle)){ ?><h2 class="single-subtitle" itemprop="description"><?php echo $subtitle; ?></h2><?php } ?>
+          <?php  if(!empty($subtitle)){ ?><h2 class="single-subtitle" itemprop="description"><?php echo esc_html($subtitle); ?></h2><?php } ?>
         <?php } else if(is_category()) { ?>
           <h1><?php echo esc_html__( 'Category: ', "healthclinic"  ); ?></h1>
         <?php } else if(is_tag()) { ?>
           <h1><?php echo esc_html__( 'Tag: ', "healthclinic"  ); ?></h1>
         <?php } else if(is_single()) { ?>
           <h1><?php the_title(); ?></h1>
-          <?php  if(!empty($subtitle)){ ?><h2 class="single-subtitle" itemprop="description"><?php echo $subtitle; ?></h2><?php } ?>
+          <?php  if(!empty($subtitle)){ ?><h2 class="single-subtitle" itemprop="description"><?php echo esc_html($subtitle); ?></h2><?php } ?>
         <?php } else { ?>
           <h1><?php the_title(); ?></h1>
-          <?php  if(!empty($subtitle)){ ?><h4 class="mt-page-subtitle" itemprop="description"><?php echo $subtitle; ?></h2><?php } ?>
+          <?php  if(!empty($subtitle)){ ?><h4 class="mt-page-subtitle" itemprop="description"><?php echo esc_html($subtitle); ?></h2><?php } ?>
         <?php } ?>
       </div>
     </div>
